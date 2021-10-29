bfc852a05bb90584fa44b76e369e4d0140f589e8
Alex Cwik <alexcwik1490@gmail.com>
2021-10-29 13:18:04 -0500

    worked on seeds but still issues with formating table

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,25 @@ CREATE DATABASE workplace_data_db;
 USE workplace_data_db;
 
 CREATE TABLE department (
-    PRIMARY KEY (id),
-    department_name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-  PRIMARY KEY (id),
-  title VARCHAR (30) NOT NULL,
-  salary DECIMAL INT NOT NULL,
-  FOREIGN KEY (department_id),
-  REFERENCES department (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-   PRIMARY KEY (id),
-   first_name VARCHAR(30) NOT NULL,
-   last_name VARCHAR(30) NOT NULL,
-   FOREIGN KEY (roles_id),
-   REFERENCES roles(id),
-   manager_id INT
-)
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
 
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+USE workplace_data_db;
+
+INSERT INTO department (name)
+VALUES 
+
+('001 Sales')
+('002 Engineering')
+('003 Finance')
+('004 Legal')
+
+INSERT INTO roles (name, salary, department)
+VALUES
+
+('001 Salesperson')
+('002 Lead Engineer')
+('003 Software Engineer')
+('004 Accountant')
+('005 Legal Team Lead')
+('006 Lawyer')
+
+INSERT INTO employee (names, salary, department, id)

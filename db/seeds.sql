4dab5109a9b397e95a4f5be1fcd42822631b82eb
Alex Cwik <alexcwik1490@gmail.com>
2021-11-01 16:03:46 -0500

    added employee names, salary, roles and id's to seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,29 @@
 USE workplace_data_db;
 
-INSERT INTO department (name)
+INSERT INTO department (department_name)
 VALUES 
 
-('001 Sales')
-('002 Engineering')
-('003 Finance')
-('004 Legal')
+('Sales')
+('Engineering')
+('Finance')
+('Legal')
 
-INSERT INTO roles (name, salary, department)
+INSERT INTO roles (title, salary, department_id)
 VALUES
 
-('001 Salesperson')
-('002 Lead Engineer')
-('003 Software Engineer')
-('004 Accountant')
-('005 Legal Team Lead')
-('006 Lawyer')
+('Salesperson', 65000, 001)
+('Lead Engineer', 100000, 002)
+('Software Engineer', 88000, 002)
+('Accountant',70000, 003)
+('Legal Team Lead', 98000, 004)
+('Lawyer', 180000, 004)
 
-INSERT INTO employee (names, salary, department, id)
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+VALUES
+
+('Thomas', 'LaGrassa', 001, 002)
+('Karen', 'Lavery', 002, 002)
+('Rachel', 'Kirk', 003, 002)
+('Bill', 'Nissius', 004, 002)
+('Marian', 'Hossa', 002, 002)
+('Dick', 'Butkus', 004, 002)
